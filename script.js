9dee162b5f644428db378501e1caf9a522505a7b
Maxime Vernay <vernaymaxime4@gmail.com>
2025-05-12 21:55:36 +0200

    affichages des points map

--- script.js
+++ script.js
@@ -14,6 +14,17 @@ function initMap(lat, lon,macarte) {
         minZoom: 1,
         maxZoom: 20
     }).addTo(macarte);
+    let marker = L.marker([lat, lon]).addTo(macarte);
+    marker.bindPopup("votre localisation");
+    for (let i = 0; i<listeRando.length;i++){
+        const locRando = listeRando[i]["randonnee_localisation"].split(";");
+        const longRando = parseFloat(locRando[1]);
+        const latRando = parseFloat(locRando[0]);
+        let marker1 = L.marker([latRando, longRando]).addTo(macarte);
+
+        
+    }
+
 }
 function initMapTrajet(lat, lon,macarte2) {
     // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
@@ -24,6 +35,10 @@ function initMapTrajet(lat, lon,macarte2) {
         attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
         minZoom: 1,
         maxZoom: 20
+    }).addTo(macarte2);
+    var gpx = '../gpx-trace3.gpx';
+    new L.GPX(gpx, {async: true}).on('loaded', function(e) {
+    map.fitBounds(e.target.getBounds());
     }).addTo(macarte2);
 }
 // window.onload = function(){
@@ -67,16 +82,17 @@ async function init(){
         const {latitude, longitude} = position.coords;
         latUser = latitude;
         longUser = longitude;
-        let macarte = null;
-        let macarte2 = null;
-        initMap(latUser,longUser,macarte); 
-        initMapTrajet(latUser,longUser,macarte2);
-
+        console.log(latUser)
         const response = await fetch('../api.php');
         data = await response.json();
         console.log(data);
 
         getproxi();
+        let macarte = null;
+        let macarte2 = null;
+        initMap(latUser,longUser,macarte); 
+        initMapTrajet(latUser,longUser,macarte2);
+
     }catch (error) {
         console.error('Erreur attrapée dans init() :', error);
     }
@@ -90,12 +106,10 @@ const latToKm = (lat) =>{  // calcul de la lat en kilometre
     return lat*111
 }
 const longToKm = (long,lat) =>{
-    return long *111*Math.cos(lat*(Math.PI/180)) //calcul de la long en kilometre
+    return long*111*Math.cos(lat*(Math.PI/180)) //calcul de la long en kilometre
 }
 
-function displayMap(){
 
-}
 
 function getproxi(){  // renvoie une liste avec les rando à moins de 10km de la position de l'user
     for (let i =0; i<data.length; i++){
@@ -107,7 +121,8 @@ function getproxi(){  // renvoie une liste avec les rando à moins de 10km de la
             listeRando.push(data[i])
         }
     }
-    displayMap()
+    console.log(listeRando)
+
 
 
 }
